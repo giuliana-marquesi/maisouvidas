58cef36485949f3d6b6ca3de439ba2acefd97f40
Giuliana Marquesi <giugianam@gmail.com>
2017-09-01 20:02:15 -0300

    faz a diferenca e adiciona numa nova lista de dicionarios

--- importaCriaImg.py
+++ importaCriaImg.py
@@ -8,8 +8,27 @@ with open('1.json') as json_data:
 with open('2.json') as json_data:
     segundo = json.load(json_data)
 
+novo_json = []
+bibl = {}
+for d in segundo:
+    for i in primeiro:
+        if d['id'] == i['id']:
+            contagem_semana = d['playback_count'] - i['playback_count']
+            print('subtracao', contagem_semana)
+            bibl = d
+            bibl['playback_count'] = contagem_semana
+            novo_json.append(bibl)
+    novo_json.append(d)
+
 
 def pegaNumeroViews(item):
     return item['playback_count']
 
-ordenado = sorted(resposta, key=pegaNumeroViews, reverse=True)
+ordenado = sorted(novo_json, key=pegaNumeroViews, reverse=True)
+
+for audio in range(0, 10):
+    titulo = ordenado[audio]['title']
+    numeroViews = str(ordenado[audio]['playback_count'])
+    numeroDownloads = str(ordenado[audio]['download_count'])
+
+    print(titulo + ' ' + numeroViews + ' ' + numeroDownloads)
