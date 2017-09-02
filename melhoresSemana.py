4357c96601a3be504203d9cf62cddb63c2da5bf2
Giuliana <giugianam@gmail.com>
2017-09-02 20:37:32 -0300

    conta correta e nome modificado

--- melhoresSemana.py
+++ melhoresSemana.py
@@ -4,7 +4,9 @@ import requests
 import json
 import codecs
 
-url = 'https://api.soundcloud.com/users/252453423/tracks?client_id=qpqr7lvfbvgt2DrSlaXoHjeiMxH99YR9&limit=200'
+novo_json = []
+
+url = 'URL'
 
 segundo = requests.get(url).json()
 
@@ -14,24 +16,24 @@ with open('1.json') as json_data:
 with codecs.open( "primeiro.json", 'w', 'latin1') as f:
     json.dump(segundo, f)
 
-novo_json = []
-bibl = {}
-
-for seg in segundo:
-    bibl = seg
-    bibl = (item for item in primeiro if item['id'] == seg['id']).next()
-    print(bibl)
-    for prim in primeiro if prim['id'] == seg['id']:
-        contagem_semana = seg['playback_count'] - prim['playback_count']
-        print('subtracao', contagem_semana)
-        bibl = seg
-        bibl['playback_count'] = contagem_semana
-        novo_json.append(bibl)
-    novo_json.append(d)
-
 
 def pegaNumeroViews(item):
     return item['playback_count']
+
+views_primeiro = 0
+
+for seg in segundo:
+    for prim in primeiro:
+        if prim['id'] == seg['id']:
+            views_primeiro = pegaNumeroViews(prim)
+            break
+        else:
+            views_primeiro = 0
+    contagem_semana = seg['playback_count'] - views_primeiro
+    seg['playback_count'] = contagem_semana
+    novo_json.append(seg)
+
+
 
 ordenado = sorted(novo_json, key=pegaNumeroViews, reverse=True)
 
