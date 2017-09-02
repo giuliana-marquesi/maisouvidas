858ebf32f2dcf2bee251ff04940bf25af0769b83
Giuliana Marquesi <giugianam@gmail.com>
2017-09-02 16:04:32 -0300

    importa primeiro, cria o segundo e exporta. For nao funcionando

--- importaCriaImg.py
+++ importaCriaImg.py
@@ -1,23 +1,32 @@
 # -*- coding: utf-8 -*-
 
+import requests
 import json
+import codecs
+
+url = 'https://api.soundcloud.com/users/252453423/tracks?client_id=qpqr7lvfbvgt2DrSlaXoHjeiMxH99YR9&limit=200'
+
+segundo = requests.get(url).json()
 
 with open('1.json') as json_data:
     primeiro = json.load(json_data)
 
-with open('2.json') as json_data:
-    segundo = json.load(json_data)
+with codecs.open( "primeiro.json", 'w', 'latin1') as f:
+    json.dump(segundo, f)
 
 novo_json = []
 bibl = {}
-for d in segundo:
-    for i in primeiro:
-        if d['id'] == i['id']:
-            contagem_semana = d['playback_count'] - i['playback_count']
-            print('subtracao', contagem_semana)
-            bibl = d
-            bibl['playback_count'] = contagem_semana
-            novo_json.append(bibl)
+
+for seg in segundo:
+    bibl = seg
+    bibl = (item for item in primeiro if item['id'] == seg['id']).next()
+    print(bibl)
+    for prim in primeiro if prim['id'] == seg['id']:
+        contagem_semana = seg['playback_count'] - prim['playback_count']
+        print('subtracao', contagem_semana)
+        bibl = seg
+        bibl['playback_count'] = contagem_semana
+        novo_json.append(bibl)
     novo_json.append(d)
 
 
