a7193fd6c11c4fb3ddd636bb2c2fd83194e2dff5
Giuliana Marquesi <giugianam@gmail.com>
2017-09-04 01:32:16 -0300

    finalizado melhoresSemana

--- melhoresSemana.py
+++ melhoresSemana.py
@@ -3,10 +3,15 @@
 import requests
 import json
 import codecs
+import texttable as tt
 
-novo_json = []
+tab = tt.Texttable()
+header = ["Título", "Reproduções", "Downloads"]
 
-url = 'URL'
+
+matriz = [["Título", "Reproduções", "Downloads"]]
+
+url = 'QUERY'
 
 segundo = requests.get(url).json()
 
@@ -16,30 +21,30 @@ with open('1.json') as json_data:
 with codecs.open( "primeiro.json", 'w', 'latin1') as f:
     json.dump(segundo, f)
 
-
 def pegaNumeroViews(item):
-    return item['playback_count']
+    return item[1]
 
 views_primeiro = 0
 
 for seg in segundo:
     for prim in primeiro:
         if prim['id'] == seg['id']:
-            views_primeiro = pegaNumeroViews(prim)
+            views_primeiro = prim['playback_count']
             break
         else:
             views_primeiro = 0
     contagem_semana = seg['playback_count'] - views_primeiro
-    seg['playback_count'] = contagem_semana
-    novo_json.append(seg)
+    matriz.append([seg['title'], contagem_semana, seg['download_count']])
 
+ordenado = sorted(matriz, key=pegaNumeroViews, reverse=True)
+ordenado = ordenado[0:11]
 
+tab.set_cols_width([100,12,10])
+tab.header(header)
+tab.add_rows(ordenado)
+tabela = tab.draw()
+print(tabela)
 
-ordenado = sorted(novo_json, key=pegaNumeroViews, reverse=True)
-
-for audio in range(0, 10):
-    titulo = ordenado[audio]['title']
-    numeroViews = str(ordenado[audio]['playback_count'])
-    numeroDownloads = str(ordenado[audio]['download_count'])
-
-    print(titulo + ' ' + numeroViews + ' ' + numeroDownloads)
+arquivo_tabela = codecs.open("melhoresSemana.md", 'a', 'utf-8')
+arquivo_tabela.write(tabela)
+arquivo_tabela.close()
