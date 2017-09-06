1c98194d57fc2a3a62edc58291e7cd5a3b2c13df
Giuliana Marquesi <giugianam@gmail.com>
2017-09-05 21:18:26 -0300

    usando o plotly

--- criadasMaisOuvidas.py
+++ criadasMaisOuvidas.py
@@ -7,12 +7,19 @@
 import codecs
 import requests
 from datetime import datetime, timedelta
-import texttable as tt
+#import texttable as tt
+import plotly.offline as offline
+import plotly.figure_factory as ff
+import sys
 
-tab = tt.Texttable()
-header = ["Título", "Reproduções", "Downloads"]
-tab.header(header)
-tab.set_cols_width([100,12,10])
+#resolve problemas com acentuação das strings, erro dado no python 2.7
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+#tab = tt.Texttable()
+#tab.header(header)
+#tab.set_cols_width([100,12,10])
+matriz = [["Título", "Reproduções", "Downloads"]]
 
 url = 'QUERY_URL'
 sete_dias_atras = datetime.now() - timedelta(days=7)
@@ -31,11 +38,17 @@ for audio in range(0, 10):
     numero_views = str(ordenado[audio]['playback_count'])
     numero_downloads = str(ordenado[audio]['download_count'])
     linha_tabela = [titulo, numero_views, numero_downloads]
-    tab.add_row(linha_tabela)
+    matriz.append(linha_tabela)
 
-tabela = tab.draw()
+tabela = ff.create_table(matriz, height_constant=20)
 
-print(tabela)
-arquivo_tabela = codecs.open("criadasMaisOuvidas.md", 'a', 'utf-8')
-arquivo_tabela.write(tabela)
-arquivo_tabela.close()
+#def adjust_column_widths(ff_table, n_cols=2, new_xs=[1.]):
+#    for i,entry in enumerate(ff_table['layout']['annotations']):
+#        print(i)
+#        if i % n_cols!=0:
+#            entry['x'] = new_xs[(i % n_cols)-1]
+#
+#adjust_column_widths(tabela)
+
+offline.plot(tabela, {'layout': {'title': 'As mais ouvidas'}},
+             image='png',link_text='um link text', output_type='file',filename='temp-plot.html', image_filename='um_teste', image_width=800, image_height=600, validate=False)
