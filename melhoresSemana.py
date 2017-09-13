ce4d39d5e1c5f892fa94cf10e18a4288fc75016d
Giuliana Marquesi <giugianam@gmail.com>
2017-09-13 16:32:13 -0300

    retomando texttable

--- melhoresSemana.py
+++ melhoresSemana.py
@@ -3,16 +3,18 @@
 import requests
 import json
 import codecs
+#import plotly.offline as offline
+#import plotly.figure_factory as ff
 import texttable as tt
 
 tab = tt.Texttable()
-matriz = [["Título", "Reproduções", "Download"]]
-url = 'QUERY'
+header = [["Título", "Reproduções", "Download"]]
+matriz = []
+url = 'QUERY_URL'
 views_primeiro = 0
 
 segundo = requests.get(url).json()
 
-#nesta semana precisa mudar o nome do arquivo para primeiro.json
 with open('1.json') as json_data:
     primeiro = json.load(json_data)
 with codecs.open( "primeiro.json", 'w', 'latin1') as f:
@@ -30,14 +32,19 @@ for seg in segundo:
 
 def pegaNumeroViews(item):
     return item[1]
+
 ordenado = sorted(matriz, key=pegaNumeroViews, reverse=True)
-ordenado = ordenado[0:11]
+ordenado = ordenado[0:10]
+
+nova_matriz = header + ordenado
+
+#tabela = ff.create_table(nova_matriz, height_constant=20)
+
+#offline.plot(tabela, {'layout': {'title': 'As mais ouvidas'}},
+#             image='png',link_text='um link text', output_type='file',filename='temp-plot.html',
+#             image_filename='um_teste', image_width=800, image_height=600, validate=False)
 
 tab.set_cols_width([100,12,10])
-tab.add_rows(ordenado)
+tab.add_rows(nova_matriz)
 tabela = tab.draw()
 print(tabela)
-
-arquivo_tabela = codecs.open("melhoresSemana.md", 'a', 'utf-8')
-arquivo_tabela.write(tabela)
-arquivo_tabela.close()
